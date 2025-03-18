2d5340c1406a6354230b40e0371e248d05a8164f
El Luco <saintsuperylucas@gmail.com>
2025-03-18 16:57:15 +0100

    init grpc

--- grpc/src/app.module.ts
+++ grpc/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Room } from './entities/room.entity';
+import { Reservation } from './entities/reservation.entity';
+import { Notif } from './entities/notif.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'pguser',
+      password: 'pgpass',
+      database: 'pgdb',
+      entities: [User, Room, Reservation, Notif],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User, Room, Reservation, Notif]), 
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
+
